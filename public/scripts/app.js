691e694e65761591643dac6530021eec20b9e787
serhan gökmeydan <gokmeydanserhan@gmail.com>
2025-03-19 21:36:16 +0300

    weather api was added to the project

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,20 +2,28 @@ const form = document.querySelector("#form");
 const geolocation = window.navigator.geolocation;
 const map = L.map("map");
 let marker;
-let coords;
+let currentCoords;
 
 //get current location
 geolocation.getCurrentPosition(async (e) => {
-  coords = {
+  currentCoords = {
     lat: e.coords.latitude,
     lon: e.coords.longitude,
   };
-  
-  //set the map view to the current location
-  map.setView([coords.lat, coords.lon], 13);
 
-  //add a marker to the map
-  marker = L.marker([coords.lat, coords.lon]).addTo(map);
+  try {
+    const response = await axios.post("/app/api", { coords: currentCoords });
+    const data = response.data;
+
+    //set the map view to the current location
+    map.setView([currentCoords.lat, currentCoords.lon], 13);
+
+    //add a marker to the map
+    marker = L.marker([currentCoords.lat, currentCoords.lon]).addTo(map);
+
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png?", {
@@ -36,85 +44,20 @@ map.on("click", onMapClick);
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const mapForm = new FormData(form);
-  const formData = {};
-  mapForm.forEach((value, key) => {
-    formData[key] = value;
-  });
-
+  const city = document.querySelector("#city").value;
   try {
-    const mapResponse = await axios.post("/map/api", formData);
-    const mapData = mapResponse.data;
+    const response = await axios.post("/app/api", { city });
+    const data = response.data;
 
-    if (Object.entries(mapData).length > 0) {
-      map.setView([mapData.lat, mapData.lon], 13);
+    if (Object.entries(data).length > 0) {
+      map.setView([data.coords.lat, data.coords.lon], 13);
       marker
-        ? marker.setLatLng([mapData.lat, mapData.lon])
-        : (marker = L.marker([mapData.lat, mapData.lon]).addTo(map));
+        ? marker.setLatLng([data.coords.lat, data.coords.lon])
+        : (marker = L.marker([data.coords.lat, data.coords.lon]).addTo(map));
     } else {
       console.log("City not found");
     }
-
   } catch (error) {
     console.error(error);
   }
 });
-
-// const geolocation = window.navigator.geolocation;
-// const map = L.map("map");
-// const popup = L.popup();
-// let marker;
-
-// // Get the current location
-// geolocation.getCurrentPosition((e) => {
-//   const coords = {
-//     latitude: e.coords.latitude,
-//     longitude: e.coords.longitude,
-//   };
-
-//   // Set the map view to the current location
-//   map.setView([coords.latitude, coords.longitude], 13);
-
-//   // Add a marker to the map
-//   marker = L.marker([coords.latitude, coords.longitude]).addTo(map);
-// });
-
-// L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png?", {
-//   maxZoom: 19,
-//   minZoom: 2,
-//   attribution:
-//     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-// }).addTo(map);
-
-// const onMapClick = (e) => {
-//   marker
-//     ? marker.setLatLng(e.latlng)
-//     : (marker = L.marker(e.latlng).addTo(map));
-//   map.panTo(e.latlng);
-// };
-// map.on("click", onMapClick);
-
-// document.querySelector("#city-form").addEventListener("submit", async (e) => {
-//   e.preventDefault();
-//   const city = document.querySelector("#city-input").value;
-//   try {
-//     const response = await axios.post("/api/city", { city });
-//     const data = response.data[0];
-
-//     if (data) {
-//       const coords = {
-//         lat: data.lat,
-//         lon: data.lon,
-//       };
-
-//       map.setView([coords.lat, coords.lon], 13);
-//       marker
-//         ? marker.setLatLng([coords.lat, coords.lon])
-//         : (marker = L.marker([coords.lat, coords.lon]).addTo(map));
-//     } else {
-//       console.log("City not found");
-//     }
-//   } catch (error) {
-//     console.error(error);
-//   }
-// });
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bodyParser from "body-parser";
+// import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
@@ -9,72 +9,44 @@ app.use(express.static("public"));
 app.use(express.static("node_modules/leaflet/dist"));
 app.use(express.static("node_modules/axios/dist"));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-const getCoordinates = async (req, res, next) => {
-  const city = req.body.city;
-  try {
-    const response = await axios.get(
-      `https://nominatim.openstreetmap.org/search?q=${city}&format=json&`
-    );
-
-    const data = response.data.filter((item) => {
-      return item.addresstype !== "province";
-    })[0];
-
-    const coords = {
-      lat: parseFloat(data.lat),
-      lon: parseFloat(data.lon),
-    };
-
-    req.coords = coords;
-    next();
-  } catch (error) {
-    res.send(error);
-  }
-};
-app.use(getCoordinates);
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.render("app.ejs");
 });
 
-app.post("/map/api", getCoordinates, (req, res) => {
-  res.json(req.coords);
+app.post("/app/api", async (req, res) => {
+
+  try {
+    const mapResponse = await axios.get(
+      `https://nominatim.openstreetmap.org/search?q=${req.body.city}&format=json&`
+    );
+
+    const mapData = mapResponse.data.filter((item) => {
+      return item.addresstype !== "province";
+    })[0];
+
+    const coords = {
+      lat: mapData.name !== "undefined" ? mapData.lat : req.body.coords.lat,
+      lon: mapData.name !== "undefined" ? mapData.lon : req.body.coords.lon,
+    };
+
+    const weatherResponse = await axios.get(
+      `https://api.open-meteo.com/v1/forecast?latitude=${coords.lat}&longitude=${coords.lon}&hourly=temperature_2m`
+    );
+    const weatherData = weatherResponse.data;
+
+    res.json({
+      coords: coords,
+      weather: weatherData,
+    });
+    
+  } catch (error) {
+    res.send(error);
+  }
 });
-
-// app.get("/", (req, res) => {
-//   res.render("index.ejs");
-// });
-
-// app.post("/api/city", async (req, res) => {
-//   const city = req.body.city;
-//   console.log(req);
-//   try {
-//     const mapResponse = await axios.get(
-//       `https://nominatim.openstreetmap.org/search?q=${city}&format=json&`
-//     );
-
-//     const mapResponseData = mapResponse.data.filter(
-//       (item) => item.addresstype !== "province"
-//     )[0];
-
-//     const lat = parseFloat(mapResponseData.lat).toFixed(2);
-//     const lon = parseFloat(mapResponseData.lon).toFixed(2);
-
-//     const weatherResponse = await axios.get(
-//       `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m&current=temperature_2m`
-//     );
-//     const weatherResponseData = weatherResponse.data;
-
-//     res.json([mapResponseData,weatherResponseData]);
-
-//     console.log(weatherResponse.data);
-//   } catch (error) {
-//     res.send(error);
-//   }
-// });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
