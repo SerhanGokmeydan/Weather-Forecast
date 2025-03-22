ab525b15fb1066e177434c1e55bea766a6b798e9
serhan gökmeydan <gokmeydanserhan@gmail.com>
2025-03-23 00:19:30 +0300

    country dropdown list and some weather display stuffes were added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,6 +3,8 @@ const geolocation = window.navigator.geolocation;
 const map = L.map("map");
 let marker;
 let currentCoords;
+const countries = document.querySelector("#countries");
+const cities = document.querySelector("#cities");
 
 //get current location
 geolocation.getCurrentPosition(async (e) => {
@@ -11,21 +13,23 @@ geolocation.getCurrentPosition(async (e) => {
     lon: e.coords.longitude,
   };
 
+  //get the weather data for the current location
   try {
-    const response = await axios.post("/app/api", { coords: currentCoords });
-    const data = response.data;
+    const data = await getApiData({ coords: currentCoords });
+    displayWeather(data.weather);
+    displayCountries(data.countriesList);
 
     //set the map view to the current location
     map.setView([currentCoords.lat, currentCoords.lon], 13);
 
     //add a marker to the map
     marker = L.marker([currentCoords.lat, currentCoords.lon]).addTo(map);
-
   } catch (error) {
     console.error(error);
   }
 });
 
+//add the OpenStreetMap tile layer to the map
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png?", {
   maxZoom: 19,
   minZoom: 2,
@@ -33,27 +37,59 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png?", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-const onMapClick = (e) => {
+//add a marker to the map when the user clicks on the map
+const onMapClick = async (e) => {
+  const coords = {
+    lat: e.latlng.lat,
+    lon: e.latlng.lng,
+  };
+
   marker
     ? marker.setLatLng(e.latlng)
     : (marker = L.marker(e.latlng).addTo(map));
   map.panTo(e.latlng);
+
+  try {
+    const data = await getApiData({ coords });
+    displayWeather(data.weather);
+  } catch (error) {
+    console.log(error);
+  }
 };
 map.on("click", onMapClick);
+
+countries.addEventListener("input", () => {
+  form.dispatchEvent(new Event("submit"));
+});
+
+cities.addEventListener("input", () => {
+  form.dispatchEvent(new Event("submit"));
+});
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const city = document.querySelector("#city").value;
   try {
-    const response = await axios.post("/app/api", { city });
-    const data = response.data;
+    //get the weather data and location for the city
+    // const data = await getApiData({ location: countries.value});
+    // console.log(data.citiesList)
+    let data;
+    if (!cities.value) {
+      data = await getApiData({ location: countries.value });
+    } else {
+      data = await getApiData({
+        location: countries.value + " " + cities.value,
+      });
+    }
+    displayCities(data.citiesList);
 
-    if (Object.entries(data).length > 0) {
+    if (Object.entries(data.coords).length > 0) {
       map.setView([data.coords.lat, data.coords.lon], 13);
       marker
         ? marker.setLatLng([data.coords.lat, data.coords.lon])
         : (marker = L.marker([data.coords.lat, data.coords.lon]).addTo(map));
+
+      displayWeather(data.weather);
     } else {
       console.log("City not found");
     }
@@ -61,3 +97,49 @@ form.addEventListener("submit", async (e) => {
     console.error(error);
   }
 });
+
+// get the api data
+const getApiData = async (data) => {
+  try {
+    const response = await axios.post("/app/api", data);
+    const result = response.data;
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//display the weather data for the city
+const displayWeather = (data) => {
+  const weatherRecords = document.querySelector("#weather-records");
+  weatherRecords.innerHTML = `
+    <ul id="weather-records-list">
+      ${Object.keys(data)
+        .map((key) => {
+          return `<li>${key}: ${data[key]}</li>`;
+        })
+        .join("")}
+    </ul>
+  `;
+};
+
+const displayCountries = (data) => {
+  const countriesList = document.querySelector("#countries");
+  countriesList.innerHTML += `
+    ${data.map((countryInfo) => {
+      return `<option ${`value = ${countryInfo.country}`}>${
+        countryInfo.country
+      }</option>`;
+    })}
+  `;
+};
+
+const displayCities = (data) => {
+  const citiesSelect = document.querySelector("#cities");
+  citiesSelect.innerHTML = `
+      <option value="" selected disabled hidden>Please, select a city</option>
+      ${data.map((city) => {
+        return `<option ${`value = ${city}`}>${city}</option>`;
+      })}
+  `;
+};
--- server.js
+++ server.js
@@ -1,27 +1,43 @@
 import express from "express";
-// import bodyParser from "body-parser";
+import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
 const port = 3000;
 
+let countriesList = [];
+let citiesList = [];
+
 app.use(express.static("public"));
 app.use(express.static("node_modules/leaflet/dist"));
 app.use(express.static("node_modules/axios/dist"));
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.render("app.ejs");
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://countriesnow.space/api/v0.1/countries"
+    );
+    countriesList = response.data.data;
+    res.render("app.ejs");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.post("/app/api", async (req, res) => {
-
   try {
+    for (let countryInfo of countriesList) {
+      if (countryInfo.country === req.body.location) {
+        citiesList = countryInfo.cities;
+      }
+    }
+
+    //get the coordinates for the city
     const mapResponse = await axios.get(
-      `https://nominatim.openstreetmap.org/search?q=${req.body.city}&format=json&`
+      `https://nominatim.openstreetmap.org/search?q=${req.body.location}&format=json&`
     );
 
     const mapData = mapResponse.data.filter((item) => {
@@ -33,16 +49,18 @@ app.post("/app/api", async (req, res) => {
       lon: mapData.name !== "undefined" ? mapData.lon : req.body.coords.lon,
     };
 
+    //get the weather data for the city
     const weatherResponse = await axios.get(
-      `https://api.open-meteo.com/v1/forecast?latitude=${coords.lat}&longitude=${coords.lon}&hourly=temperature_2m`
+      `https://api.open-meteo.com/v1/forecast?latitude=${coords.lat}&longitude=${coords.lon}&daily=weather_code,sunshine_duration,uv_index_max,rain_sum,sunrise,sunset,daylight_duration,snowfall_sum,showers_sum,precipitation_sum&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,rain,showers,snowfall,snow_depth,weather_code,surface_pressure,visibility,wind_speed_10m,wind_direction_10m,cloud_cover,precipitation&current=temperature_2m,weather_code,wind_speed_10m,relative_humidity_2m,rain,cloud_cover,wind_direction_10m,apparent_temperature,showers,pressure_msl,is_day,snowfall,surface_pressure,precipitation&timezone=GMT`
     );
     const weatherData = weatherResponse.data;
 
     res.json({
       coords: coords,
       weather: weatherData,
+      countriesList: countriesList,
+      citiesList: citiesList,
     });
-    
   } catch (error) {
     res.send(error);
   }
